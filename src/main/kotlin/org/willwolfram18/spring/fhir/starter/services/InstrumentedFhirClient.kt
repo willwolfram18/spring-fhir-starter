3dd77cc1af3717ba06d3da23e4d5473302a11e4d
William Wolfram <willwolfram18@gmail.com>
2026-02-22 22:06:02 -0800

    Refactored InstrumentedFhirClient for DRY code
    Refactored some parts of the FHIR client's code for tracing and metrics into a reusable method to make adding and maintaining the methods easier to work with

--- src/main/kotlin/org/willwolfram18/spring/fhir/starter/services/InstrumentedFhirClient.kt
+++ src/main/kotlin/org/willwolfram18/spring/fhir/starter/services/InstrumentedFhirClient.kt
@@ -25,126 +25,42 @@ class InstrumentedFhirClient(
         fhirClient.fhirContext.version.version.name
     }
 
-    fun <T : Resource> search(resource: Class<T>, queryBuilder: (IQuery<Bundle>.() -> IQuery<Bundle>) = { this }): Bundle {
+    fun <T : Resource> search(
+        resource: Class<T>,
+        queryBuilder: (IQuery<Bundle>.() -> IQuery<Bundle>) = { this }
+    ): Bundle {
         val operationName = operation(resource, "Search")
-        val spanBuilder = newSpan(operationName)
 
-        if (tracer.currentSpan() != null) {
-            spanBuilder.setParent(tracer.currentSpan()!!.context())
-        }
-
-        val searchSpan = spanBuilder.start()
-        // start a Micrometer Timer sample so we can record duration with outcome-tag afterwards
-        val runningTimer = Timer.start(meterRegistry)
-        var outcome = "success"
-        var result: Bundle
-        tracer.withSpan(searchSpan).use {
+        return executeWithTelemetry(operationName) {
             val query = fhirClient.search<Bundle>()
                 .forResource(resource)
                 .run(queryBuilder)
 
-            try {
-                result = query.returnBundle(Bundle::class.java).execute()
-            } catch (e: Exception) {
-                outcome = "error"
-                searchSpan.tag("outcome", outcome.uppercase())
-                    .error(e)
-                throw e
-            } finally {
-                searchSpan.end()
-                // Register (or get) a Timer with the operation and outcome tags and stop the sample
-                val timer = Timer.builder("fhir.client.request.duration")
-                    .description("Duration of FHIR client operations")
-                    .publishPercentiles(0.5, 0.9, 0.95, 0.99)
-                    .tags("operation", operationName, "fhir_version", fhirVersion, "outcome", outcome.uppercase())
-                    .register(meterRegistry)
-
-                runningTimer.stop(timer)
-            }
+            query.returnBundle(Bundle::class.java).execute()
         }
-
-        return result
     }
 
     fun <T : Resource> create(resource: T, block: (ICreateTyped.() -> ICreateTyped) = { this }): MethodOutcome? {
         val operationName = operation(resource.javaClass, "Create")
-        val spanBuilder = newSpan(operationName)
 
-        if (tracer.currentSpan() != null) {
-            spanBuilder.setParent(tracer.currentSpan()!!.context())
-        }
-
-        val searchSpan = spanBuilder.start()
-        // start a Micrometer Timer sample so we can record duration with outcome-tag afterwards
-        val runningTimer = Timer.start(meterRegistry)
-        var outcome = "success"
-        var result: MethodOutcome
-        tracer.withSpan(searchSpan).use {
+        return executeWithTelemetry(operationName) {
             val createRequest = fhirClient.create()
                 .resource(resource)
                 .run(block)
 
-            try {
-                result = createRequest.execute()
-            } catch (e: Exception) {
-                outcome = "error"
-                searchSpan.tag("outcome", outcome.uppercase())
-                    .error(e)
-                throw e
-            } finally {
-                searchSpan.end()
-                // Register (or get) a Timer with the operation and outcome tags and stop the sample
-                val timer = Timer.builder("fhir.client.request.duration")
-                    .description("Duration of FHIR client operations")
-                    .publishPercentiles(0.5, 0.9, 0.95, 0.99)
-                    .tags("operation", operationName, "fhir_version", fhirVersion, "outcome", outcome.uppercase())
-                    .register(meterRegistry)
-
-                runningTimer.stop(timer)
-            }
+            createRequest.execute()
         }
-
-        return result
     }
 
     fun <T : Resource> read(resource: Class<T>, block: IReadTyped<T>.() -> IReadExecutable<T>): T {
         val operationName = operation(resource, "Read")
-        val spanBuilder = newSpan(operationName)
 
-        if (tracer.currentSpan() != null) {
-            spanBuilder.setParent(tracer.currentSpan()!!.context())
+        return executeWithTelemetry(operationName) {
+            fhirClient.read()
+                .resource(resource)
+                .run(block)
+                .execute()
         }
-
-        val searchSpan = spanBuilder.start()
-        // start a Micrometer Timer sample so we can record duration with outcome-tag afterwards
-        val runningTimer = Timer.start(meterRegistry)
-        var outcome = "success"
-        var result: T
-        tracer.withSpan(searchSpan).use {
-            try {
-                result = fhirClient.read()
-                    .resource(resource)
-                    .run(block)
-                    .execute()
-            } catch (e: Exception) {
-                outcome = "error"
-                searchSpan.tag("outcome", outcome.uppercase())
-                    .error(e)
-                throw e
-            } finally {
-                searchSpan.end()
-                // Register (or get) a Timer with the operation and outcome tags and stop the sample
-                val timer = Timer.builder("fhir.client.request.duration")
-                    .description("Duration of FHIR client operations")
-                    .publishPercentiles(0.5, 0.9, 0.95, 0.99)
-                    .tags("operation", operationName, "fhir_version", fhirVersion, "outcome", outcome.uppercase())
-                    .register(meterRegistry)
-
-                runningTimer.stop(timer)
-            }
-        }
-
-        return result
     }
 
     private fun operation(resource: Class<*>, name: String): String =
@@ -155,5 +71,58 @@ class InstrumentedFhirClient(
         // TODO consider using "host name instead of full serverBase
         .remoteServiceName(fhirClient.serverBase)
         .name(operation)
-        .tag("fhir.version", fhirVersion)
+        .tag("fhir_version", fhirVersion)
+
+    private fun <R> executeWithTelemetry(operationName: String, block: () -> R): R =
+        executeWithTracing(operationName) {
+            executeWithTimer(operationName) {
+                block()
+            }
+        }
+
+    // Helper that handles tracing/span lifecycle and error tagging.
+    private fun <R> executeWithTracing(operationName: String, block: () -> R): R {
+        val spanBuilder = newSpan(operationName)
+
+        if (tracer.currentSpan() != null) {
+            spanBuilder.setParent(tracer.currentSpan()!!.context())
+        }
+
+        val span = spanBuilder.start()
+        tracer.withSpan(span).use {
+            try {
+                return block()
+            } catch (e: Exception) {
+                span.error(e)
+                throw e
+            } finally {
+                span.end()
+            }
+        }
+    }
+
+    // Helper that handles Micrometer Timer lifecycle and outcome tagging for metrics.
+    private fun <R> executeWithTimer(operationName: String, block: () -> R): R {
+        val runningTimer = Timer.start(meterRegistry)
+        var outcome = "SUCCESS"
+
+        try {
+            return block()
+        } catch (e: Exception) {
+            outcome = "ERROR"
+            throw e
+        } finally {
+            val timer = Timer.builder("fhir.client.request.duration")
+                .description("Duration of FHIR client operations")
+                .publishPercentiles(0.5, 0.9, 0.95, 0.99)
+                .tags(
+                    "operation", operationName,
+                    "fhir_version", fhirVersion,
+                    "outcome", outcome
+                )
+                .register(meterRegistry)
+
+            runningTimer.stop(timer)
+        }
+    }
 }
