da87cb64c319291941edb4c5df0cb2b5b02f10b7
William Wolfram <willwolfram18@gmail.com>
2026-02-22 17:11:23 -0800

    Functioning metrics for search

--- src/main/kotlin/org/willwolfram18/spring/fhir/starter/services/InstrumentedFhirClient.kt
+++ src/main/kotlin/org/willwolfram18/spring/fhir/starter/services/InstrumentedFhirClient.kt
@@ -3,6 +3,7 @@
 import ca.uhn.fhir.rest.client.api.IGenericClient
 import ca.uhn.fhir.rest.gclient.IQuery
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
 import io.micrometer.tracing.Span
 import io.micrometer.tracing.Tracer
 import org.hl7.fhir.r4.model.Bundle
@@ -17,17 +18,21 @@ class InstrumentedFhirClient(
 ) {
 
     fun <T : Resource> search(resource: Class<T>, queryBuilder: IQuery<Bundle>.() -> IQuery<Bundle>): Bundle {
+        val fhirOperation = "${resource.simpleName}.Search (${fhirClient.fhirContext.version.version.name})"
         val spanBuilder = tracer.spanBuilder()
             .kind(Span.Kind.CLIENT)
             // TODO consider using "host name instead of full serverBase
             .remoteServiceName(fhirClient.serverBase)
-            .name("${resource.simpleName}.Search (${fhirClient.fhirContext.version.version.name})")
+            .name(fhirOperation)
 
         if (tracer.currentSpan() != null) {
             spanBuilder.setParent(tracer.currentSpan()!!.context())
         }
 
         val searchSpan = spanBuilder.start()
+        // start a Micrometer Timer sample so we can record duration with outcome-tag afterwards
+        val runningTimer = Timer.start(meterRegistry)
+        var outcome = "success"
         var result: Bundle
         tracer.withSpan(searchSpan).use {
             val query = fhirClient.search<Bundle>()
@@ -38,10 +43,19 @@ class InstrumentedFhirClient(
             try {
                 result = query.returnBundle(Bundle::class.java).execute()
             } catch (e: Exception) {
+                outcome = "error"
                 searchSpan.error(e)
                 throw e
             } finally {
                 searchSpan.end()
+                // Register (or get) a Timer with the operation and outcome tags and stop the sample
+                val timer = Timer.builder("fhir.client.request.duration")
+                    .description("Duration of FHIR client operations")
+                    .publishPercentiles(0.5, 0.9, 0.95, 0.99)
+                    .tags("operation", fhirOperation, "outcome", outcome)
+                    .register(meterRegistry)
+
+                runningTimer.stop(timer)
             }
         }
 
